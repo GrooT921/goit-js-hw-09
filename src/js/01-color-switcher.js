20c4b0d7bb90e02340e2edea9a4482826f8fb65a
GrooT921 <rarapin96@gmail.com>
2022-08-05 18:48:02 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,27 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
-// ReferenceError
+let intervalId = null;
+startBtn.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick(event) {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  // console.log(event.target);
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+stopBtn.addEventListener('click', onBtnStopClick);
+
+function onBtnStopClick(event) {
+  // console.log(event.target);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(intervalId);
+}
