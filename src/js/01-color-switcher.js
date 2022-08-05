3f1acbbf0eb4409b23234806438224057896a27d
GrooT921 <rarapin96@gmail.com>
2022-08-05 18:51:19 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,8 +4,8 @@ const body = document.querySelector('body');
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
 let intervalId = null;
+
 startBtn.addEventListener('click', onStartBtnClick);
 
 function onStartBtnClick(event) {
